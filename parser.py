93898c90ab49c14ecf2a19cfbc4bb489f1407888
Andy Kwong <yantkwong@gmail.com>
2021-01-17 15:26:10 -0800

    further parse the tweets into dictionary

--- parser.py
+++ parser.py
@@ -3,5 +3,19 @@ def get_tweets(statuses):
 
     for status in statuses:
         tweets.append(dict(text=status.text,
-                           created_at=status.created_at.strftime("%m/%d/%Y, %H:%M:%S")))
+                           created_at=status.created_at.strftime("%m/%d/%Y")))
     return tweets
+
+
+def get_date_dictionary(tweets):
+    date_dictionary = dict()
+
+    for tweet in tweets:
+        tweet_date = tweet["created_at"]
+
+        if tweet_date in date_dictionary.keys():
+            date_dictionary[tweet_date] += ' ' + tweet["text"]
+        else:
+            date_dictionary[tweet_date] = tweet["text"]
+
+    return date_dictionary
